b57cc4c3ed279338a17e33e54e05ba631a3a0b46
Adam Miller <miad0801@gmail.com>
2019-10-06 16:32:50 -0500

    Added friends for survey

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+// Friends data
+
+var friends = [
+	
+    {
+        name:"Ahmed",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+        ]
+    },
+	{
+		name: "Kobe",
+		photo: "https://usatftw.files.wordpress.com/2014/06/usp-nba_-los-angeles-lakers-at-sacramento-kings.jpg?w=1000&h=600&crop=1",
+		scores: [
+		"3",
+		"1",
+		"5",
+		"4",
+		"1",
+		"5",
+		"1",
+		"1",
+		"5",
+		"3"
+		]
+	},
+	{
+		name: "Mack",
+		photo: "https://cdn.images.express.co.uk/img/dynamic/4/590x/secondary/1554023.jpg?r=1539536698561",
+		score: [
+		"3",
+		"1",
+		"4",
+		"1",
+		"4",
+		"3",
+		"5",
+		"1",
+		"5",
+		"4"		
+		]
+	}
+];
+
+module.exports = friends;
